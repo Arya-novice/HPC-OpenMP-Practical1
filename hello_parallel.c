6c05b0a3b634c4803fcc5ae7681f9bb7f68e89b4
Arya-novice <arya.ninawe@walchandsangli.ac.in>
2025-07-31 09:02:58 +0000

    Added OpenMP practical files

--- hello_parallel.c
+++ hello_parallel.c
@@ -7,10 +7,21 @@ int main() {
     scanf("%d", &n);
     omp_set_num_threads(n);
 
+    double start = omp_get_wtime();
+
     #pragma omp parallel
     {
         int tid = omp_get_thread_num();
         printf("Hello, World from thread %d\n", tid);
     }
+
+    double end = omp_get_wtime();
+    double par_time = end - start;
+
+    printf("Parallel Execution Time: %f seconds\n", par_time);
+    double speedup = seq_time / par_time;
+    printf("Speed-Up = %f\n", speedup);
+
+
     return 0;
 }
--- hello_sequential.c
+++ hello_sequential.c
@@ -1,12 +1,20 @@
-
 #include <stdio.h>
+#include <omp.h>
 
 int main() {
     int n;
     printf("Enter number of threads: ");
+
+    double start = omp_get_wtime();
+
     scanf("%d", &n);
     for(int i = 0; i < n; i++) {
         printf("Hello, World from thread %d\n", i);
     }
+
+    double end = omp_get_wtime();
+    double seq_time = end - start;
+
+    printf("Sequential Execution Time: %f seconds\n", seq_time);
     return 0;
 }
